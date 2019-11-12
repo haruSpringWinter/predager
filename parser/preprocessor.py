249e61fabf5cd44dfd28f6c5bcc358dc496eeb2d
haruSpringWinter <haruru1428@yahoo.co.jp>
2019-11-13 00:29:01 +0900

    add featurization process

--- parser/preprocessor.py
+++ parser/preprocessor.py
@@ -1,7 +1,9 @@
 import MeCab
 import numpy as np
+from pyspark import RDD
+from pyspark.sql import DataFrame, Row
 
-def to_morph(sentence):
+def to_morph(sentence: str) -> np.array:
     m = MeCab.Tagger("-Ochasen")
     node = m.parseToNode(sentence)
     list = []
@@ -10,3 +12,23 @@ def to_morph(sentence):
         node = node.next
         list.append(word)
     return np.array(list)
+
+def to_some_feature(sentence: str):
+    # TODO: determine featurization process and do it
+    pass
+
+def convert_df_to_feature(df: DataFrame) -> RDD:
+    feature_df = df.rdd.map(
+        lambda row: row_to_feature(row)
+    )
+
+    return feature_df
+
+# Row(age, sex, sentence, url)
+def row_to_feature(row: Row) -> Row:
+    sentence = row['sencence']
+    age = row['age']
+    sex = row['sex']
+    morph = to_morph(sentence)
+
+    Row(('age', age), ('sex', sex), ('feat', morph))
