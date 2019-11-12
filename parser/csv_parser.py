37c16bf9391f58d26785aca9c551e800fcab4abc
HarukaAbe <abe@miubiq.cs.titech.ac.jp>
2019-11-12 23:32:29 +0900

    refine parser

--- parser/csv_parser.py
+++ parser/csv_parser.py
@@ -1,6 +1,28 @@
 import numpy as np
-import pandas as pd
+from typing import *
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession, DataFrame
+from pyspark import SparkContext, SparkConf
 
-def parse(path):
-    df = pd.read_csv(path)
-    return np.array(df)
+conf = SparkConf().setAll(
+        # TODO: define values by environment variable rather than hard-coding
+        [
+            ('spark.executor.memory', '4g'),
+            ('spark.driver.memory', '4g'),
+            ('spark.cores.max', '4')
+        ]
+    )
+sc = SparkContext(conf = conf)
+ss = SparkSession(sc)
+
+# age,sex,sentence,ur
+twitter_schema = StructType([
+    StructField('age', IntegerType(), False),
+    StructField('sex', StringType(), False),
+    StructField('sentence', StringType(), False),
+    StructField('url', StringType(), False)
+])
+
+def load_as_df(path: str) -> DataFrame:
+    df = ss.read.csv(path, header = True, schema = twitter_schema)
+    return df
