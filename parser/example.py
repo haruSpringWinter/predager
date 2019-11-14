6f7b57a1d217c6eae54f52ac13b3ec8afe6ffb34
haruSpringWinter <haruru1428@yahoo.co.jp>
2019-11-14 23:08:14 +0900

    update example

--- parser/example.py
+++ parser/example.py
@@ -3,40 +3,51 @@ import preprocessor
 import MeCab
 import numpy as np
 from pyspark.sql import Row, DataFrame
+from pyspark import RDD
 
-def convert_df_to_feature(df: DataFrame) -> DataFrame:
-    feature_df = df.rdd.map(
+def convert_df_to_feature(df: DataFrame) -> RDD:
+    feature_df = df.rdd.filter(
+        lambda row: row['sentence'] != None
+    )
+
+    rdd = feature_df.map(
         lambda row: row_to_feature(row)
     )
 
-    return feature_df
+    return rdd 
 
-def to_morph(sentence: str) -> np.array:
+
+def to_morph(sentence: str) -> list:
     m = MeCab.Tagger("-Ochasen")
     node = m.parseToNode(sentence)
-    list = []
+    node = node.next # skip BOF
+    node_list = []
     while node:
-        word = node.surface
+        # word = node.surface
         node = node.next
-        list.append(word)
-    return str(list)
+        if node != None:
+            feat = node.feature
+            surface = node.surface
+            node_list.append((surface, feat))
+    return node_list
 
-# Row(age, sex, sentence, url)
-def row_to_feature(row: Row) -> Row:
+
+def row_to_feature(row: Row) -> tuple:
     sentence = row['sentence']
     age = row['age']
     sex = row['sex']
     morph = to_morph(sentence)
-
-    feature_row = Row(('age', age), ('sex', sex), ('feat', morph))
+    # feature_row = Row(('age', age), ('sex', sex), ('feat', morph))
+    feature_row = (age, sex, morph)
     return feature_row
+
 
 path = 'example_data/20190528sentences_data_integrated.csv'
 df = csv_parser.load_as_df(path)
 df.show(3)
 
+
 converted = convert_df_to_feature(df)
-print(converted.count())
 sample = converted.take(3)
 for e in sample:
     print(e)
