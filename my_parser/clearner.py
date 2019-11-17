e6b3404c1a486093f35ced2b7d31d791902db815
haruSpringWinter <haruru1428@yahoo.co.jp>
2019-11-17 18:16:58 +0900

    update clearer

--- my_parser/clearner.py
+++ my_parser/clearner.py
@@ -2,16 +2,17 @@ import re
 
 from bs4 import BeautifulSoup
 
-
 def clean_text(text: str) -> str:
-    # replaced_text = '\n'.join(s.strip() for s in text.splitlines()[2:] if s != '')  # skip header by [2:]
     replaced_text = '\n'.join(text.splitlines())  # skip header by [2:]
+    replaced_text = clean_url(replaced_text)
+    replaced_text = clean_html_tags(replaced_text)
+    replaced_text = clean_html_and_js_tags(replaced_text)
     replaced_text = replaced_text.lower()
     replaced_text = re.sub(r'[【】]', ' ', replaced_text)       # 【】の除去
     replaced_text = re.sub(r'[（）()]', ' ', replaced_text)     # （）の除去
     replaced_text = re.sub(r'[［］\[\]]', ' ', replaced_text)   # ［］の除去
     replaced_text = re.sub(r'[@＠]\w+', '', replaced_text)  # メンションの除去
-    replaced_text = re.sub(r'https?:\/\/.*?[\r\n ]', '', replaced_text)  # URLの除去
+    replaced_text = re.sub(r'https?:\/\/.*.?[\r\n ]?', '', replaced_text)  # URLの除去
     replaced_text = re.sub(r'　', ' ', replaced_text)  # 全角空白の除去
     return replaced_text
 
@@ -41,29 +42,18 @@ def clean_url(html_text: str) -> str:
     return clean_text
 
 
-def clean_code(html_text: str) -> str:
-    """Qiitaのコードを取り除きます
-    :param html_text:
-    :return:
-    """
-    soup = BeautifulSoup(html_text, 'html.parser')
-    [x.extract() for x in soup.findAll(class_="code-frame")]
-    cleaned_text = soup.get_text()
-    cleaned_text = ''.join(cleaned_text.splitlines())
-    return cleaned_text
-
 if __name__ == "__main__":
-    print("abcdefg".splitlines()[2:])
-
     example_txts = [
         "@picachu 草でチュウ",
-        "This is url http://hoge.com",
+        "This is url https://hoge",
         "year <span color=\"red\"> wvec </span>",
-        "normal"
+        "normal",
+        "【草】草草草の草【朗報】"
     ]
 
     cleaned_txts = [clean_text(txt) for txt in example_txts]
 
     for txt, cleaned in zip(example_txts, cleaned_txts):
-        print(txt)
-        print(cleaned)
+        print("Given:   " + txt)
+        print("Cleaned: " + cleaned)
+        print()
