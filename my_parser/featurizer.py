3f5e28e6c0abeb2134c84e871066a18b67da25ad
haruSpringWinter <haruru1428@yahoo.co.jp>
2019-11-17 19:28:19 +0900

    vectarize sentence

--- my_parser/featurizer.py
+++ my_parser/featurizer.py
@@ -3,6 +3,9 @@ import numpy as np
 import normalizer, stopwords_handler, clearner
 from pyspark import RDD
 from pyspark.sql import DataFrame, Row
+from gensim.corpora import Dictionary
+
+dct = Dictionary()
 
 
 def to_morph(sentence: str) -> list:
@@ -21,27 +24,63 @@ def to_morph(sentence: str) -> list:
     return node_list
 
 
-def row_to_feature(row: Row, n: int = 100, min_freq: int = 1) -> tuple:
+def update_dictionary(nodes: list) -> None:
+    global dct
+
+    surfaces = []
+    for node in nodes:
+        surfaces.append(node[0])
+
+    dct.add_documents([surfaces])    
+
+
+def preprocess(row: Row, n: int = 100, min_freq: int = 1, for_test = False) -> tuple:
+    global dct
     sentence = row['sentence']
     clean_text = clearner.clean_text(sentence)
     normalized_text = normalizer.normalize(clean_text)
     nodes = to_morph(normalized_text)
-    cleaned = stopwords_handler.remove_stopnodes(nodes)
+
+    if for_test:
+        nodes = [node for node in nodes if node[0] in dct]
+
+    cleaned_nodes = stopwords_handler.remove_stopnodes(nodes)
+    update_dictionary(cleaned_nodes)
 
     age = row['age']
     sex = row['sex']
 
     # feature_row = Row(('age', age), ('sex', sex), ('feat', morph))
-    feature_row = (age, sex, cleaned)
+    preprocessed = (age, sex, cleaned_nodes)
 
-    return feature_row
+    return preprocessed
 
 
-def convert_df_to_feature(df: DataFrame) -> RDD:
-    feature_rdd = df.rdd.filter(
+def convert_row_to_feature_vec(row: tuple) -> tuple:
+    global dct
+
+    dim = len(dct)
+    nodes = row[2]
+    terms = [node[0] for node in nodes]
+    vecs = []
+    for term in terms:
+        vec = [0 for i in range(dim)]
+        vec[dct.token2id[term]] += 1
+        vecs.append(vec)
+    # TODO: Add some features
+    
+    return (row[0], row[1], vecs)
+
+
+def convert_df_to_feature(df: DataFrame, for_test = False) -> RDD:
+    preproc_rdd = df.rdd.filter(
         lambda row: row['sentence'] != None
     ).map(
-        lambda row: row_to_feature(row)
+        lambda row: preprocess(row)
+    )
+
+    feature_rdd = preproc_rdd.map(
+        lambda row: convert_row_to_feature_vec(row)
     )
 
     return feature_rdd
@@ -54,8 +93,8 @@ def convert_df_to_feature(df: DataFrame) -> RDD:
 2.  DONE: 形態素解析
 3.  DONE: クリーニング
 4.  DONE: 正規化 (ストップワードの除去含む)
-5.  TODO: 辞書作成 (単語とIDの対応づけ) https://qiita.com/tatsuya-miyamoto/items/f505dfa8d5307f8c6e98　簡単にできそう
-6.  TODO: ベクトル化 (埋め込み or IDからone-hot)
+5.  DONE: 辞書作成 (単語とIDの対応づけ) https://qiita.com/tatsuya-miyamoto/items/f505dfa8d5307f8c6e98　簡単にできそう
+6.  DONE: ベクトル化 (埋め込み or IDからone-hot)
 7.  TODO: 文章特徴抽出 (文章の長さなど) 
 8.  TODO: 提案特徴抽出 (フォロー/フォロワーの特徴)
 9.  TODO: 識別モデルの実装
