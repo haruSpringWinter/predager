12744480f5ce0c81af73424231436877ca14b9e2
haruSpringWinter <haruru1428@yahoo.co.jp>
2019-12-16 23:26:21 +0900

    add super

--- nn/models.py
+++ nn/models.py
@@ -3,12 +3,13 @@ import torch
 
 class MLP(nn.Module):
     def __init__(self, input_dim, num_hidden, out_dim):
+        super(MLP, self).__init__()
         self.input_dim = input_dim
         self.num_hidden = num_hidden
         self.out_dim = out_dim
-
         self.layers = nn.Sequential(
             nn.Linear(input_dim, num_hidden),
+            nn.BatchNorm1d(num_hidden)
             nn.ReLU(),
             nn.Linear(num_hidden, out_dim)
         )
