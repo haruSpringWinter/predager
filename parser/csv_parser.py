176436b09c5bf0637ed7c4d0ffd0de1889f64183
haruSpringWinter <haruru1428@yahoo.co.jp>
2021-05-22 16:24:14 +0900

    Feat: Use config parser

--- parser/csv_parser.py
+++ parser/csv_parser.py
@@ -1,16 +1,15 @@
-import numpy as np
-from typing import *
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession, DataFrame
 from pyspark import SparkContext, SparkConf
-from schema.twitter_schema import twitter_schema
+from schema import twitter_schema
+from utils.predager_config import predager_config
 
 conf = SparkConf().setAll(
     # TODO: define values by environment variable rather than hard-coding
     [
-        ('spark.executor.memory', '4g'),
-        ('spark.driver.memory', '4g'),
-        ('spark.cores.max', '4')
+        ('spark.executor.memory', predager_config.spark.executor.get('memory')),
+        ('spark.driver.memory', predager_config.spark.driver.get('memory')),
+        ('spark.cores.max', predager_config.spark.cores.get('max'))
     ]
 )
 sc = SparkContext(conf=conf)
